c70ec5e12005b58951e793c9afde4ae51a677bcb
Roman Sorokin <roman@amperka.ru>
2016-06-03 16:57:36 +0300

    fixes

--- js/atmosphere.js
+++ js/atmosphere.js
@@ -3,14 +3,18 @@ var Atmosphere = function(callback) {
 
 };
 
-Atmosphere.prototype.pressure = function(altitude, T, Pzero) {
+Atmosphere.prototype.pressure = function(longitude, latitude, altitude, T, Pzero) {
+    // TODO: Брать давление из данных сервера
+    
     Pzero = Pzero || Consts.Pzero;
     return Pzero * Math.pow(Math.E, -1 * Consts.M.air * Consts.G * altitude / (Consts.R * T));
 };
 
-Atmosphere.prototype.temperature = function(altitude, Tzero) {
+Atmosphere.prototype.temperature = function(longitude, latitude, altitude) {
+    // TODO: Брать температуру из данных сервера
     
-}
+    
+};
 
 Atmosphere.prototype.get = function(longitude, latitude, altitude) {
     
